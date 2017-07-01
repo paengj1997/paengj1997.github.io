deee2e42da8f5989720a267414a8c2175e35d62a
Peeraya <cha16066@byui.edu>
2017-07-01 11:25:48 -0600

    add

--- weather/html/search.js
+++ weather/html/search.js
@@ -1,3 +1,5 @@
+var returned;
+
 $('#query').keyup(function() {
     var value = $('#query').val();
     var rExp = new RegExp(value, "i");
@@ -8,7 +10,7 @@ $('#query').keyup(function() {
         $.each(data.RESULTS, function(key, val) {
             if (val.name.search(rExp) != -1) {
                 output += '<li>';
-                output += '<a href="//www.wunderground.com' + val.l + '" title="See results for ' + val.name + '">' + val.name + '</a>';
+                output += '<a href="//www.wunderground.com"' + ' onclick="getData(' + val.lat + ',' + val.lon + ')"' + ' title="See results for ' + val.name + '">' + val.name + '</a>';
                 output += '</li>';
             }
         }); // end each
@@ -16,3 +18,44 @@ $('#query').keyup(function() {
         $("#searchResults").html(output); // send results to the page
     }); // end getJSON
 }); // end onkeyup
+
+
+$("#searchResults").on("click", "a", function (evt) {
+    evt.preventDefault();
+//    $()
+    // With the text value get the needed value from the weather.json file
+    var jsonCity = $(this).text(); // Franklin, etc...
+    console.log(jsonCity);
+    index = $(this).index("a");
+
+//    getData(returned.RESULTS[index].zmw);
+    getData(index.lat, index.lon);
+
+
+    $('#searchResults').hide();
+//    document.getElementById('searchResults').style.display='none';
+});
+function getData(lat, lon){
+    $.ajax({
+        url:"https://api.wunderground.com/api/a8791e5a6a538fa0/geolookup/conditions/q/" + lat + ',' + lon + ".json",
+        dataType : "jsonp",
+        success : function(parsed_json) {
+            var location = parsed_json['location']['city'];
+            var temp_f = parsed_json['current_observation']['temp_f'];
+            $('#currentTemp').html(Math.round(temp_f) +  " &#8457" );
+
+            $("title").text(parsed_json.current_observation.display_location.full);
+            $("#cityDisplay").text(parsed_json.current_observation.display_location.full);
+            $("#summary").text(parsed_json.current_observation.weather);
+            $("#add1").html(" Feels like  " + Math.round(parsed_json.current_observation.feelslike_f) + " &#8457");
+            $("#add2").text("Humidity:  " + parsed_json.current_observation.relative_humidity);
+            $("#add3").html("Wind from the " + parsed_json.current_observation.wind_dir + " at " + parsed_json.current_observation.wind_mph + " mph");
+
+            $("#cover").fadeOut(250);
+
+            console.log(parsed_json);
+        }
+
+    });
+
+}
